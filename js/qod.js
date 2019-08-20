0184c8bf823c070bd62cc5f6e705f2221985b960
Matt Waraich <mattsinghwaraich@gmail.com>
2019-08-20 11:49:29 -0700

    second commit

---
+++ js/qod.js
@@ -0,0 +1,12 @@
+(function($) {
+  $(function() {
+      // TODO remove line of code below
+    $('body').append('hello world');
+
+    //ajax and other JS code will go here 
+
+    //1: get request for wp/v2/posts
+
+    //2: post request for wp/v2/posts
+  }); //end of doc ready
+})(jQuery);
